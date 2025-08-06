89197e6c9d3be42413cec52218c2cae50bd9faa4
mahmoud-tests-git <mahmoudnoarab@gmail.com>
2025-08-07 02:41:51 +0300

    chore: login page

---
+++ src/app/[locales]/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from '@/components/organisms/LoginForm';
+
+export default function LoginPage() {
+  return (
+    <div className="container max-w-2xl mx-auto flex flex-col gap-10 mt-5 pb-10">
+      <LoginForm />
+    </div>
+  );
+}
