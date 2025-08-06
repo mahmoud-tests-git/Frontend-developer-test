b53da66e6097d3762ff2fdc06a4e68d69b9b513c
mahmoud-tests-git <mahmoudnoarab@gmail.com>
2025-08-07 02:42:52 +0300

    fix: layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
+import '@/styles/globals.css';
+import { TooltipProvider } from '@radix-ui/react-tooltip';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -25,9 +26,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased  max-w-screen-2xl overflow-x-hidden mx-auto`}
       >
-        {children}
+        <TooltipProvider>{children}</TooltipProvider>
       </body>
     </html>
   );
