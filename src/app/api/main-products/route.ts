04b315eb6279a0055443b3a43dd47fed1ba08481
mahmoud-tests-git <mahmoudnoarab@gmail.com>
2025-08-07 17:19:50 +0300

    feat: improve the error handling for the api route

--- src/app/api/main-products/route.ts
+++ src/app/api/main-products/route.ts
@@ -1,20 +1,30 @@
 import fs from 'fs';
 import path from 'path';
-export async function GET(request: Request) {
-  const data = fs.readFileSync(
-    path.join(process.cwd(), 'public/mock/main-products.json'),
-    'utf8',
-  );
+export async function GET() {
+  try {
+    const filePath = path.join(process.cwd(), 'public/mock/main-products.json');
 
-  if (!data) {
-    return new Response(JSON.stringify({ error: 'No data found' }), {
-      status: 404,
-      headers: { 'Content-Type': 'application/json' },
+    const fileIsHere = fs.existsSync(filePath);
+
+    if (!fileIsHere) {
+      return new Response(JSON.stringify({ error: 'File not found' }), {
+        status: 404,
+        headers: { 'Content-Type': 'application/json' },
+      });
+    }
+    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
+    return new Response(data, {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Cache-Control': 'public, max-age=3600, stale-while-revalidate=86400',
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    return new Response(JSON.stringify({ error: 'Internal server error' }), {
+      status: 500,
     });
   }
-
-  return new Response(data, {
-    status: 200,
-    headers: { 'Content-Type': 'application/json' },
-  });
 }
