a48ba5bbd9ee2508d66fd1c8aaad86babbe90d76
mahmoud-tests-git <mahmoudnoarab@gmail.com>
2025-08-07 16:01:31 +0300

    fix(responsive) : edit fav page grid

--- src/app/[locales]/favourites/page.tsx
+++ src/app/[locales]/favourites/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
         </CardHeader>
         <CardContent>
           {favourites.length > 0 ? (
-            <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 lg:gap-10 gap-6 place-items-center">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 lg:gap-10 gap-6 place-items-center">
               {favourites.map((product) => (
                 <ProductCard
                   id={product.id}
